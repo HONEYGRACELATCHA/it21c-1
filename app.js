430b840aef59a290006d11f84147fc133c58c465
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-03 14:23:33 +0800

    simple object/class and index.html

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-console.log("My name is Honey");
-console.log("My name is bee");
+//Input
+const person1 ={
+    name:"Hhon2",
+    age:19,
+    occupation:"None"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
