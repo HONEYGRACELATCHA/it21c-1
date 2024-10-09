0dc8979fb3ff7750ef39535dd5802a87ef988d4c
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-09 22:21:32 +1100

    Class Person

---
+++ Inheritance.js
@@ -0,0 +1,13 @@
+class Person {
+
+    //Private Fields
+    #name;
+    #age;
+    #hobbies;
+
+    constructor(name, age, hobbies) {
+        this.#name = name;
+        this.#age = age;
+        this.#hobbies = hobbies;
+    }
+}
