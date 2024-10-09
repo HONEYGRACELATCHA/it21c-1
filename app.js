5e25b212acd63c298c00a63b91013fc8ca993384
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-09 18:56:38 +1100

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Input
+const person1 ={
+    name:"Honey Grace Latcha",
+    age:19,
+    hobbies: "Daydreaming"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Hobbies:"+person1.hobbies);
