19c39da40eb16602ed2da0756a437229248cd679
HONEYGRACELATCHA <gracelatcha@gmail.com>
2024-10-09 18:59:09 +1100

    Added Class Person

---
+++ AccessModifiersandEncapsulation.js
@@ -0,0 +1,14 @@
+class Person {
+
+    //Private Fields
+    #name;
+    #age;
+    #gender;
+    #hobby;
+
+    constructor(name, age, hobby) {
+        this.#name = name;
+        this.#age = age;
+        this.#hobby = hobby;
+    }
+}
